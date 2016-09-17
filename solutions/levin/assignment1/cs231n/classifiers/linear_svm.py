ef24754590592a3d69b7ae0e001865f5add6228b
Levin Jian <jianzhirong@gmail.com>
2016-09-17 16:33:59 +0800

    complete svm model implementation

--- solutions/levin/assignment1/cs231n/classifiers/linear_svm.py
+++ solutions/levin/assignment1/cs231n/classifiers/linear_svm.py
@@ -2,91 +2,117 @@ import numpy as np
 from random import shuffle
 
 def svm_loss_naive(W, X, y, reg):
-  """
-  Structured SVM loss function, naive implementation (with loops).
+    """
+    Structured SVM loss function, naive implementation (with loops).
 
-  Inputs have dimension D, there are C classes, and we operate on minibatches
-  of N examples.
+    Inputs have dimension D, there are C classes, and we operate on minibatches
+    of N examples.
 
-  Inputs:
-  - W: A numpy array of shape (D, C) containing weights.
-  - X: A numpy array of shape (N, D) containing a minibatch of data.
-  - y: A numpy array of shape (N,) containing training labels; y[i] = c means
-    that X[i] has label c, where 0 <= c < C.
-  - reg: (float) regularization strength
+    Inputs:
+    - W: A numpy array of shape (D, C) containing weights.
+    - X: A numpy array of shape (N, D) containing a minibatch of data.
+    - y: A numpy array of shape (N,) containing training labels; y[i] = c means
+        that X[i] has label c, where 0 <= c < C.
+    - reg: (float) regularization strength
 
-  Returns a tuple of:
-  - loss as single float
-  - gradient with respect to weights W; an array of same shape as W
-  """
-  dW = np.zeros(W.shape) # initialize the gradient as zero
+    Returns a tuple of:
+    - loss as single float
+    - gradient with respect to weights W; an array of same shape as W
+    """
+    dW = np.zeros(W.shape) # initialize the gradient as zero
 
-  # compute the loss and the gradient
-  num_classes = W.shape[1]
-  num_train = X.shape[0]
-  loss = 0.0
-  for i in xrange(num_train):
-    scores = X[i].dot(W)
-    correct_class_score = scores[y[i]]
-    for j in xrange(num_classes):
-      if j == y[i]:
-        continue
-      margin = scores[j] - correct_class_score + 1 # note delta = 1
-      if margin > 0:
-        loss += margin
+    # compute the loss and the gradient
+    num_classes = W.shape[1]
+    num_train = X.shape[0]
+    loss = 0.0
+    for i in xrange(num_train):
+        scores = X[i].dot(W)
+        correct_class_score = scores[y[i]]
+        for j in xrange(num_classes):
+            
+            if j == y[i]:
+                continue
+            margin = scores[j] - correct_class_score + 1 
+            # note delta = 1
+            if margin > 0:
+                loss += margin
+                dW[:,j] = dW[:,j] + (X[i])
+                dW[:,y[i]] = dW[:,y[i]] + (-X[i])
 
-  # Right now the loss is a sum over all training examples, but we want it
-  # to be an average instead so we divide by num_train.
-  loss /= num_train
+    # Right now the loss is a sum over all training examples, but we want it
+    # to be an average instead so we divide by num_train.
+    loss /= num_train
+    dW /= num_train
+    dW += reg * W
 
-  # Add regularization to the loss.
-  loss += 0.5 * reg * np.sum(W * W)
+    # Add regularization to the loss.
+    loss += 0.5 * reg * np.sum(W * W)
 
-  #############################################################################
-  # TODO:                                                                     #
-  # Compute the gradient of the loss function and store it dW.                #
-  # Rather that first computing the loss and then computing the derivative,   #
-  # it may be simpler to compute the derivative at the same time that the     #
-  # loss is being computed. As a result you may need to modify some of the    #
-  # code above to compute the gradient.                                       #
-  #############################################################################
+    #############################################################################
+    # TODO:                                                                                                                                         #
+    # Compute the gradient of the loss function and store it dW.                                #
+    # Rather that first computing the loss and then computing the derivative,     #
+    # it may be simpler to compute the derivative at the same time that the         #
+    # loss is being computed. As a result you may need to modify some of the        #
+    # code above to compute the gradient.                                                                             #
+    #############################################################################
 
 
-  return loss, dW
+    return loss, dW
 
 
 def svm_loss_vectorized(W, X, y, reg):
-  """
-  Structured SVM loss function, vectorized implementation.
+    """
+    Structured SVM loss function, vectorized implementation.
 
-  Inputs and outputs are the same as svm_loss_naive.
-  """
-  loss = 0.0
-  dW = np.zeros(W.shape) # initialize the gradient as zero
+    Inputs and outputs are the same as svm_loss_naive.
+    """
+    loss = 0.0
+    dW = np.zeros(W.shape) # initialize the gradient as zero
 
-  #############################################################################
-  # TODO:                                                                     #
-  # Implement a vectorized version of the structured SVM loss, storing the    #
-  # result in loss.                                                           #
-  #############################################################################
-  pass
-  #############################################################################
-  #                             END OF YOUR CODE                              #
-  #############################################################################
+    #############################################################################
+    # TODO:                                                                                                                                         #
+    # Implement a vectorized version of the structured SVM loss, storing the        #
+    # result in loss.                                                                                                                     #
+    #############################################################################
+    scores = X.dot(W)
+    correct_scores = scores[range(y.shape[0]), y]
+    margins = scores - correct_scores[:,np.newaxis] + 1
+    
+    margins[np.nonzero(margins <0)] = 0
+    # exclude correct scores columns
+    margins[range(y.shape[0]), y] = 0
+    loss = np.mean(np.sum(margins, axis = 1))
+    reg_term = W * W
+    reg_term = np.sum(reg_term)
+    reg_term = 0.5 * reg *reg_term
+    loss = loss + reg_term
+#     loss += 0.5 * reg * np.sum(W * W)
+    #############################################################################
+    #                                                         END OF YOUR CODE                                                            #
+    #############################################################################
 
 
-  #############################################################################
-  # TODO:                                                                     #
-  # Implement a vectorized version of the gradient for the structured SVM     #
-  # loss, storing the result in dW.                                           #
-  #                                                                           #
-  # Hint: Instead of computing the gradient from scratch, it may be easier    #
-  # to reuse some of the intermediate values that you used to compute the     #
-  # loss.                                                                     #
-  #############################################################################
-  pass
-  #############################################################################
-  #                             END OF YOUR CODE                              #
-  #############################################################################
+    #############################################################################
+    # TODO:                                                                                                                                         #
+    # Implement a vectorized version of the gradient for the structured SVM         #
+    # loss, storing the result in dW.                                                                                     #
+    #                                                                                                                                                     #
+    # Hint: Instead of computing the gradient from scratch, it may be easier        #
+    # to reuse some of the intermediate values that you used to compute the         #
+    # loss.                                                                                                                                         #
+    #############################################################################
+    
+    temp_matrix = np.zeros(scores.shape)
+    temp_matrix[np.nonzero(margins >0)] = 1 # corresponds to j != y[i]
+    temp_matrix[range(y.shape[0]), y] = -np.sum(temp_matrix, axis = 1) #corresponds to j == y[i]
+    dW = np.dot(X.T, temp_matrix)
+    
+    
+    dW /= y.shape[0]
+    dW += reg * W
+    #############################################################################
+    #                                                         END OF YOUR CODE                                                            #
+    #############################################################################
 
-  return loss, dW
+    return loss, dW
--- solutions/levin/assignment1/svm/svmmodel.py
+++ solutions/levin/assignment1/svm/svmmodel.py
@@ -9,7 +9,11 @@ import numpy as np
 from assignment1.cs231n.data_utils import load_CIFAR10
 import matplotlib.pyplot as plt
 from assignment1.cs231n import data_utils
-from sklearn.cross_validation import KFold
+from assignment1.cs231n.classifiers.linear_svm import svm_loss_naive
+from assignment1.cs231n.gradient_check import grad_check_sparse
+import time
+from assignment1.cs231n.classifiers.linear_svm import svm_loss_vectorized
+from assignment1.cs231n.classifiers.linear_classifier import LinearSVM
 
 
 class SVModel(object):
@@ -130,9 +134,195 @@ class SVModel(object):
         self.substract_mean()
         self.append_bias()
         return
+    def compute_loss_grad_naive(self):
+        # Evaluate the naive implementation of the loss we provided for you
+        # generate a random SVM weight matrix of small numbers
+        W = np.random.randn(3073, 10) * 0.0001 
+        
+        loss, grad = svm_loss_naive(W, self.X_dev, self.y_dev, 0.00001)
+        print 'loss: %f' % (loss, )
+        #Once you've implemented the gradient, recompute it with the code below
+        # and gradient check it with the function we provided for you
+        
+        # Compute the loss and its gradient at W.
+        loss, grad = svm_loss_naive(W, self.X_dev, self.y_dev, 0.0)
+        
+        # Numerically compute the gradient along several randomly chosen dimensions, and
+        # compare them with your analytically computed gradient. The numbers should match
+        # almost exactly along all dimensions.
+        
+        f = lambda w: svm_loss_naive(w, self.X_dev, self.y_dev, 0.0)[0]
+        grad_numerical = grad_check_sparse(f, W, grad)
+        
+        # do the gradient check once again with regularization turned on
+        # you didn't forget the regularization gradient did you?
+        loss, grad = svm_loss_naive(W, self.X_dev, self.y_dev, 1e2)
+        f = lambda w: svm_loss_naive(w, self.X_dev, self.y_dev, 1e2)[0]
+        grad_numerical = grad_check_sparse(f, W, grad)
+        return
+    def vectorize_loss_computation(self):
+        # Next implement the function svm_loss_vectorized; for now only compute the loss;
+        # we will implement the gradient in a moment.
+        W = np.random.randn(3073, 10) * 0.0001
+        tic = time.time()
+        loss_naive, grad_naive = svm_loss_naive(W, self.X_dev, self.y_dev, 0.00001)
+        toc = time.time()
+        print 'Naive loss: %e computed in %fs' % (loss_naive, toc - tic)
+        
+        
+        tic = time.time()
+        loss_vectorized, _ = svm_loss_vectorized(W, self.X_dev, self.y_dev, 0.00001)
+        toc = time.time()
+        print 'Vectorized loss: %e computed in %fs' % (loss_vectorized, toc - tic)
+        
+        # The losses should match but your vectorized implementation should be much faster.
+        print 'difference: %f' % (loss_naive - loss_vectorized)
+        return
+    def vectorize_grad_computation(self):
+        # Complete the implementation of svm_loss_vectorized, and compute the gradient
+        # of the loss function in a vectorized way.
+        
+        # The naive implementation and the vectorized implementation should match, but
+        # the vectorized version should still be much faster.
+        W = np.random.randn(3073, 10) * 0.0001
+        tic = time.time()
+        _, grad_naive = svm_loss_naive(W, self.X_dev, self.y_dev, 0.00001)
+        toc = time.time()
+        print 'Naive loss and gradient: computed in %fs' % (toc - tic)
+        
+        tic = time.time()
+        _, grad_vectorized = svm_loss_vectorized(W, self.X_dev, self.y_dev, 0.00001)
+        toc = time.time()
+        print 'Vectorized loss and gradient: computed in %fs' % (toc - tic)
+        
+        # The loss is a single number, so it is easy to compare the values computed
+        # by the two implementations. The gradient on the other hand is a matrix, so
+        # we use the Frobenius norm to compare them.
+        difference = np.linalg.norm(grad_naive - grad_vectorized, ord='fro')
+        print 'difference: %f' % difference
+        return
+    def use_gradient_descent(self):
+        # In the file linear_classifier.py, implement SGD in the function
+        # LinearClassifier.train() and then run it with the code below.
+        
+        svm = LinearSVM()
+        tic = time.time()
+        loss_hist = svm.train(self.X_train, self.y_train, learning_rate=2e-7, reg=1e4,
+                              num_iters=1500, verbose=True)
+#         loss_hist = svm.train(self.X_train, self.y_train, learning_rate=1e-7, reg=5e4,
+#                               num_iters=1500, verbose=True)
+        toc = time.time()
+        print 'That took %fs' % (toc - tic)
+        # A useful debugging strategy is to plot the loss as a function of
+        # iteration number:
+        plt.plot(loss_hist)
+        plt.xlabel('Iteration number')
+        plt.ylabel('Loss value')
+        plt.show()
+        # Write the LinearSVM.predict function and evaluate the performance on both the
+        # training and validation set
+        y_train_pred = svm.predict(self.X_train)
+        print 'training accuracy: %f' % (np.mean(self.y_train == y_train_pred), )
+        y_val_pred = svm.predict(self.X_val)
+        print 'validation accuracy: %f' % (np.mean(self.y_val == y_val_pred), )
+        return
+    def parameter_tuning(self):
+        learning_rates = [1e-7, 2e-7]
+        regularization_strengths = [5e4, 1e4]
+        # results is dictionary mapping tuples of the form
+        # (learning_rate, regularization_strength) to tuples of the form
+        # (training_accuracy, validation_accuracy). The accuracy is simply the fraction
+        # of data points that are correctly classified.
+        results = {}
+        best_val = -1   # The highest validation accuracy that we have seen so far.
+        best_svm = None # The LinearSVM object that achieved the highest validation rate.
+        num_iters = 1500
+        for learning_rate in learning_rates:
+            for regularization_strength in regularization_strengths:
+                print "learning_rage {}, regularization_strength {}".format(learning_rate, regularization_strength)
+                #train it
+                svm = LinearSVM()
+                svm.train(self.X_train, self.y_train, learning_rate=learning_rate, reg=regularization_strength,
+                              num_iters=num_iters, verbose=True)
+                #predict
+                y_train_pred = svm.predict(self.X_train)
+                training_accuracy = np.mean(self.y_train == y_train_pred)
+                y_val_pred = svm.predict(self.X_val)
+                validation_accuracy = np.mean(self.y_val == y_val_pred)
+                results[(learning_rate,regularization_strength)] = training_accuracy, validation_accuracy
+                if validation_accuracy > best_val:
+                    best_val = validation_accuracy
+                    best_svm = svm
+                
+        # Print out results.
+        for lr, reg in sorted(results):
+            train_accuracy, val_accuracy = results[(lr, reg)]
+            print 'lr %e reg %e train accuracy: %f val accuracy: %f' % (
+                        lr, reg, train_accuracy, val_accuracy)
+            
+        print 'best validation accuracy achieved during cross-validation: %f' % best_val
+        self.visualize_crossvaliaton(results)
+        self.evaluate_bestsvm(best_svm)
+        self.visualize_bestsvm_weights(best_svm)
+        return
+    def evaluate_bestsvm(self, best_svm):
+        # Evaluate the best svm on test set
+        y_test_pred = best_svm.predict(self.X_test)
+        test_accuracy = np.mean(self.y_test == y_test_pred)
+        print 'linear SVM on raw pixels final test set accuracy: %f' % test_accuracy
+        return
+    def visualize_bestsvm_weights(self,best_svm):
+        # Visualize the learned weights for each class.
+        # Depending on your choice of learning rate and regularization strength, these may
+        # or may not be nice to look at.
+        w = best_svm.W[:-1,:] # strip out the bias
+        w = w.reshape(32, 32, 3, 10)
+        w_min, w_max = np.min(w), np.max(w)
+        classes = ['plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+        for i in xrange(10):
+            plt.subplot(2, 5, i + 1)
+            
+            # Rescale the weights to be between 0 and 255
+            wimg = 255.0 * (w[:, :, :, i].squeeze() - w_min) / (w_max - w_min)
+            plt.imshow(wimg.astype('uint8'))
+            plt.axis('off')
+            plt.title(classes[i])
+        plt.show()
+        return
+    def visualize_crossvaliaton(self, results):
+        # Visualize the cross-validation results
+        import math
+        x_scatter = [math.log10(x[0]) for x in results]
+        y_scatter = [math.log10(x[1]) for x in results]
+        
+        # plot training accuracy
+        marker_size = 100
+        colors = [results[x][0] for x in results]
+        plt.subplot(2, 1, 1)
+        plt.scatter(x_scatter, y_scatter, marker_size, c=colors)
+        plt.colorbar()
+        plt.xlabel('log learning rate')
+        plt.ylabel('log regularization strength')
+        plt.title('CIFAR-10 training accuracy')
+        
+        # plot validation accuracy
+        colors = [results[x][1] for x in results] # default size of markers is 20
+        plt.subplot(2, 1, 2)
+        plt.scatter(x_scatter, y_scatter, marker_size, c=colors)
+        plt.colorbar()
+        plt.xlabel('log learning rate')
+        plt.ylabel('log regularization strength')
+        plt.title('CIFAR-10 validation accuracy')
+        plt.show()
+        return
     def run(self):
         self.load_data()
         self.preprocess()
+        self.parameter_tuning()
+#         self.use_gradient_descent()
+#         self.compute_loss_grad_naive()
+#         self.vectorize_loss_computation()
+#         self.vectorize_grad_computation()
 #         self.visualize_data() 
         
      
