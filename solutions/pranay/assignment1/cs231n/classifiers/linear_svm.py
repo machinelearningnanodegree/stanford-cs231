4685745111270c7fd1409075d7f0be2cff527897
pranay <paryal@blackstonemedia.com>
2016-09-28 11:10:36 -0400

    1. Softmax and linear svm files

--- solutions/pranay/assignment1/cs231n/classifiers/linear_svm.py
+++ solutions/pranay/assignment1/cs231n/classifiers/linear_svm.py
@@ -3,54 +3,50 @@ from random import shuffle
 
 def svm_loss_naive(W, X, y, reg):
   """
-  Structured SVM loss function, naive implementation (with loops).
-
-  Inputs have dimension D, there are C classes, and we operate on minibatches
-  of N examples.
-
+  Structured SVM loss function, naive implementation (with loops)
   Inputs:
-  - W: A numpy array of shape (D, C) containing weights.
-  - X: A numpy array of shape (N, D) containing a minibatch of data.
-  - y: A numpy array of shape (N,) containing training labels; y[i] = c means
-    that X[i] has label c, where 0 <= c < C.
+  - W: C x D array of weights
+  - X: D x N array of data. Data are D-dimensional columns
+  - y: 1-dimensional array of length N with labels 0...K-1, for K classes
   - reg: (float) regularization strength
-
-  Returns a tuple of:
+  Returns:
+  a tuple of:
   - loss as single float
   - gradient with respect to weights W; an array of same shape as W
   """
-  dW = np.zeros(W.shape) # initialize the gradient as zero
+  dW = np.zeros(W.shape)  # initialize the gradient as zero
 
   # compute the loss and the gradient
-  num_classes = W.shape[1]
-  num_train = X.shape[0]
+  num_classes = W.shape[0]
+  num_train = X.shape[1]
   loss = 0.0
   for i in xrange(num_train):
-    scores = X[i].dot(W)
-    correct_class_score = scores[y[i]]
-    for j in xrange(num_classes):
-      if j == y[i]:
-        continue
-      margin = scores[j] - correct_class_score + 1 # note delta = 1
-      if margin > 0:
-        loss += margin
+      scores = W.dot(X[:, i])
+      correct_class_score = scores[y[i]]
+      for j in xrange(num_classes):
+          if j == y[i]:
+              continue
+          margin = scores[j] - correct_class_score + 1  # note delta = 1
+          if margin > 0:
+              loss += margin
+
+              # Compute gradients (one inner and one outer sum)
+              # Wonderfully compact and hard to read
+              dW[y[i], :] -= X[:, i].T  # this is really a sum over j != y_i
+              dW[j, :] += X[:, i].T  # sums each contribution of the x_i's
 
   # Right now the loss is a sum over all training examples, but we want it
   # to be an average instead so we divide by num_train.
   loss /= num_train
 
-  # Add regularization to the loss.
+  # Same with gradient
+  dW /= num_train
+
+  # Add regularization
   loss += 0.5 * reg * np.sum(W * W)
 
-  #############################################################################
-  # TODO:                                                                     #
-  # Compute the gradient of the loss function and store it dW.                #
-  # Rather that first computing the loss and then computing the derivative,   #
-  # it may be simpler to compute the derivative at the same time that the     #
-  # loss is being computed. As a result you may need to modify some of the    #
-  # code above to compute the gradient.                                       #
-  #############################################################################
-
+  # Gradient regularization that carries through per https://piazza.com/class/i37qi08h43qfv?cid=118
+  dW += reg * W
 
   return loss, dW
 
@@ -62,31 +58,65 @@ def svm_loss_vectorized(W, X, y, reg):
   Inputs and outputs are the same as svm_loss_naive.
   """
   loss = 0.0
-  dW = np.zeros(W.shape) # initialize the gradient as zero
+  dW = np.zeros(W.shape)  # initialize the gradient as zero
 
   #############################################################################
-  # TODO:                                                                     #
   # Implement a vectorized version of the structured SVM loss, storing the    #
   # result in loss.                                                           #
   #############################################################################
-  pass
-  #############################################################################
-  #                             END OF YOUR CODE                              #
-  #############################################################################
+
+  # Get dims
+  D = X.shape[0]
+  num_classes = W.shape[0]
+  num_train = X.shape[1]
+  scores = W.dot(X)
+
+  # Construct correct_scores vector that is Dx1 (or 1xD) so we can subtract out
+  # where we append the "true" scores: [W*X]_{y_1, 1}, [W*X]_{y_2, 2}, ..., [W*X]_{y_D, D}
+  # Using advanced indexing into scores: http://docs.scipy.org/doc/numpy/reference/arrays.indexing.html
+  # Slow, sadly:
+  # correct_scores = np.diag(scores[y,:])
+  # Fast (index in both directions):
+  correct_scores = scores[y, np.arange(num_train)]  # using the fact that all elements in y are < C == num_classes
 
 
-  #############################################################################
-  # TODO:                                                                     #
-  # Implement a vectorized version of the gradient for the structured SVM     #
-  # loss, storing the result in dW.                                           #
-  #                                                                           #
-  # Hint: Instead of computing the gradient from scratch, it may be easier    #
-  # to reuse some of the intermediate values that you used to compute the     #
-  # loss.                                                                     #
-  #############################################################################
-  pass
-  #############################################################################
-  #                             END OF YOUR CODE                              #
-  #############################################################################
+  mat = scores - correct_scores + 1  # like above, delta = 1
+
+  mat[y, np.arange(
+      num_train)] = 0  # accounting for the j=y_i term we shouldn't count (subtracting 1 makes up for it since w_j = w_{y_j} in this case)
+
+
+  # Compute max
+  thresh = np.maximum(np.zeros((num_train)), mat)
+
+  # Compute loss as double sum
+  loss = np.sum(thresh)
+  loss /= num_train
+
+  # Add regularization
+  loss += 0.5 * reg * np.sum(W * W)
+
+
+
+  # Binarize into integers
+  binary = thresh
+  binary[thresh > 0] = 1
+
+
+  # Perform the two operations simultaneously
+  # (1) for all j: dW[j,:] = sum_{i, j produces positive margin with i} X[:,i].T
+  # (2) for all i: dW[y[i],:] = sum_{j != y_i, j produces positive margin with i} -X[:,i].T
+  col_sum = np.sum(binary, axis=0)
+
+
+  binary[y, range(num_train)] = -col_sum[range(num_train)]
+
+  dW = np.dot(binary, X.T)
+
+  # Divide
+  dW /= num_train
+
+  # Regularize
+  dW += reg * W
 
   return loss, dW
