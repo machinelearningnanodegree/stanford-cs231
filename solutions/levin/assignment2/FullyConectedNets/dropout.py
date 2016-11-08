f2cd47b5926248acb7715a80dc95b12db59b8fef
Levin Jian <jianzhirong@gmail.com>
2016-11-08 13:08:34 +0800

    update dropout notebook

--- solutions/levin/assignment2/FullyConectedNets/dropout.py
+++ solutions/levin/assignment2/FullyConectedNets/dropout.py
@@ -136,11 +136,66 @@ class Dropout(object):
                 print '%s relative error: %.2e' % (name, self.rel_error(grad_num, grads[name]))
                 print
         return
+    def experiment_regularization(self):
+        # Train two identical nets, one with dropout and one without
+        data = self.data
+        num_train = 500
+        small_data = {
+          'X_train': data['X_train'][:num_train],
+          'y_train': data['y_train'][:num_train],
+          'X_val': data['X_val'],
+          'y_val': data['y_val'],
+        }
+        
+        solvers = {}
+        dropout_choices = [0, 0.75]
+        for dropout in dropout_choices:
+            model = FullyConnectedNet([500], dropout=dropout)
+            print dropout
+            
+            solver = Solver(model, small_data,
+                            num_epochs=25, batch_size=100,
+                            update_rule='adam',
+                            optim_config={
+                              'learning_rate': 5e-4,
+                            },
+                            verbose=True, print_every=100)
+            solver.train()
+            solvers[dropout] = solver
+        # Plot train and validation accuracies of the two models
+
+        train_accs = []
+        val_accs = []
+        for dropout in dropout_choices:
+            solver = solvers[dropout]
+            train_accs.append(solver.train_acc_history[-1])
+            val_accs.append(solver.val_acc_history[-1])
+        
+        plt.subplot(3, 1, 1)
+        for dropout in dropout_choices:
+            plt.plot(solvers[dropout].train_acc_history, 'o', label='%.2f dropout' % dropout)
+        plt.title('Train accuracy')
+        plt.xlabel('Epoch')
+        plt.ylabel('Accuracy')
+        plt.legend(ncol=2, loc='lower right')
+          
+        plt.subplot(3, 1, 2)
+        for dropout in dropout_choices:
+            plt.plot(solvers[dropout].val_acc_history, 'o', label='%.2f dropout' % dropout)
+        plt.title('Val accuracy')
+        plt.xlabel('Epoch')
+        plt.ylabel('Accuracy')
+        plt.legend(ncol=2, loc='lower right')
+        
+        plt.gcf().set_size_inches(15, 15)
+        plt.show()
+        return
     def run(self):
         self.get_CIFAR10_data()
 #         self.check_dropout_forward()
 #         self.check_dropout_backward()
-        self.check_fullconn_withdropout()
+#         self.check_fullconn_withdropout()
+        self.experiment_regularization()
         return
 
 
